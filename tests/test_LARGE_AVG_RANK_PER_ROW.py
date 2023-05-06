0d2128b160681c0dba543057d1eb718200df2e6c
Brett-Kennedy <84815610+Brett-Kennedy@users.noreply.github.com>
2023-05-06 18:58:11 -0400

    Add files via upload

---
+++ tests/test_LARGE_AVG_RANK_PER_ROW.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import random
+
+import sys
+sys.path.insert(1, '..')
+from check_data_consistency import DataConsistencyChecker
+
+from utils import synth_test, synth_test_all_cols, kropt_test
+
+test_id = 'LARGE_AVG_RANK_PER_ROW'
+random.seed(0)
+
+synth_patterns_cols = []
+synth_exceptions_cols = ['"large_avg_rank_rand_0" AND "large_avg_rank_rand_1" AND "large_avg_rank_rand_2" AND "large_avg_rank_rand_3" AND "large_avg_rank_rand_4" AND "large_avg_rank_rand_5" AND "large_avg_rank_rand_6" AND "large_avg_rank_rand_7" AND "large_avg_rank_rand_8" AND "large_avg_rank_rand_9"']
+
+
+def test_synthetic_no_nulls():
+	synth_test(
+		test_id,
+		'none',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_one_row_nulls():
+	synth_test(
+		test_id,
+		'one-row',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_in_sync_nulls():
+	synth_test(
+		test_id,
+		'in-sync',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_random_nulls():
+	synth_test(
+		test_id,
+		'random',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_80_percent_nulls():
+	synth_test(
+		test_id,
+		'80-percent',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_no_nulls():
+	synth_test_all_cols(
+		test_id,
+		'none',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_one_row_nulls():
+	synth_test_all_cols(
+		test_id,
+		'one-row',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_in_sync_nulls():
+	synth_test_all_cols(
+		test_id,
+		'in-sync',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_random_nulls():
+	synth_test_all_cols(
+		test_id,
+		'random',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_80_percent_nulls():
+	synth_test_all_cols(
+		test_id,
+		'80-percent',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_fetch_kropt():
+	kropt_test(
+		test_id,
+		[],
+		[]
+	)
---
+++ tests/test_LINEAR_REGRESSION.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import random
+
+import sys
+sys.path.insert(1, '..')
+from check_data_consistency import DataConsistencyChecker
+
+from utils import synth_test, synth_test_all_cols, kropt_test
+
+test_id = 'LINEAR_REGRESSION'
+random.seed(0)
+
+synth_patterns_cols = ['"lin regr 1a" AND "lin regr 1b" AND "lin regr 1c" AND "lin regr 2" AND "lin regr 2"']
+synth_exceptions_cols = ['"lin regr 1d" AND "lin regr 1e" AND "lin regr 1f" AND "lin regr 3" AND "lin regr 3"']
+
+
+def test_synthetic_no_nulls():
+	synth_test(
+		test_id,
+		'none',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_one_row_nulls():
+	synth_test(
+		test_id,
+		'one-row',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_in_sync_nulls():
+	synth_test(
+		test_id,
+		'in-sync',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_random_nulls():
+	synth_test(
+		test_id,
+		'random',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_80_percent_nulls():
+	synth_test(
+		test_id,
+		'80-percent',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_no_nulls():
+	synth_test_all_cols(
+		test_id,
+		'none',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_one_row_nulls():
+	synth_test_all_cols(
+		test_id,
+		'one-row',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_in_sync_nulls():
+	synth_test_all_cols(
+		test_id,
+		'in-sync',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_random_nulls():
+	synth_test_all_cols(
+		test_id,
+		'random',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_80_percent_nulls():
+	synth_test_all_cols(
+		test_id,
+		'80-percent',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_fetch_kropt():
+	kropt_test(
+		test_id,
+		[],
+		[]
+	)
