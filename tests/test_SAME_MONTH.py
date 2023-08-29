33f87d39eecd5de0a271debf6ccdd1117a76dd6b
Brett-Kennedy <84815610+Brett-Kennedy@users.noreply.github.com>
2023-08-29 18:53:16 -0400

    Add files via upload

---
+++ tests/test_SAME_MONTH.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import random
+
+import sys
+sys.path.insert(1, '..')
+from check_data_consistency import DataConsistencyChecker
+
+from utils import synth_test, synth_test_all_cols, real_test, build_default_results
+
+test_id = 'SAME_MONTH'
+random.seed(0)
+
+synth_patterns_cols = ['"same_month rand" AND "same_month all"']
+synth_exceptions_cols = ['"same_month rand" AND "same_month most"',
+                         '"same_month all" AND "same_month most"']
+
+
+def test_real():
+	res = build_default_results()
+	real_test(test_id, res)
+
+
+def test_synthetic_no_nulls():
+	synth_test(
+		test_id,
+		'none',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_one_row_nulls():
+	synth_test(
+		test_id,
+		'one-row',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_in_sync_nulls():
+	synth_test(
+		test_id,
+		'in-sync',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_random_nulls():
+	synth_test(
+		test_id,
+		'random',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_80_percent_nulls():
+	synth_test(
+		test_id,
+		'80-percent',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_no_nulls():
+	synth_test_all_cols(
+		test_id,
+		'none',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_one_row_nulls():
+	synth_test_all_cols(
+		test_id,
+		'one-row',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_in_sync_nulls():
+	synth_test_all_cols(
+		test_id,
+		'in-sync',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_random_nulls():
+	synth_test_all_cols(
+		test_id,
+		'random',
+		synth_patterns_cols,
+		synth_exceptions_cols)
+
+
+def test_synthetic_all_cols_80_percent_nulls():
+	synth_test_all_cols(
+		test_id,
+		'80-percent',
+		synth_patterns_cols,
+		synth_exceptions_cols)
